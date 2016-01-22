4241f37024c78d3fa0a19dd71fd05195a226d50e
Pierre Pronchery <khorben@defora.org>
2016-01-22 02:44:25 +0100

    Re-generate the Makefiles

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PACKAGE	= DeforaOS-Artwork
-VERSION	= 0.1.1
+VERSION	= 0.1.2
 SUBDIRS	= data src
 RM	= rm -f
 LN	= ln -f
--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 PACKAGE="DeforaOS-Artwork"
-VERSION="0.1.1"
+VERSION="0.1.2"
 
 PREFIX="/usr/local"
 LIBDIR="${PREFIX}/lib"
