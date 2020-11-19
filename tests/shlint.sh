07af8971922a82fc3762ecf254e548c686ada0f9
Pierre Pronchery <khorben@defora.org>
2020-11-19 03:19:31 +0100

    Import the newer scripts from DeforaOS configure

--- tests/shlint.sh
+++ tests/shlint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$Id$
-#Copyright (c) 2014-2016 Pierre Pronchery <khorben@defora.org>
+#Copyright (c) 2014-2020 Pierre Pronchery <khorben@defora.org>
 #
 #Redistribution and use in source and binary forms, with or without
 #modification, are permitted provided that the following conditions are met:
@@ -25,23 +25,27 @@
 
 
 #variables
+CONFIGSH="${0%/shlint.sh}/../config.sh"
 PROGNAME="shlint.sh"
 PROJECTCONF="../project.conf"
 #executables
 DATE="date"
 DEBUG="_debug"
 FIND="find"
+MKDIR="mkdir -p"
 SHLINT="sh -n"
 SORT="sort -n"
 TR="tr"
+
+[ -f "$CONFIGSH" ] && . "$CONFIGSH"
 
 
 #functions
 #shlint
 _shlint()
 {
-	ret=0
-	subdirs="data doc src tests tools"
+	res=0
+	subdirs=
 
 	$DATE
 	echo
@@ -56,19 +60,23 @@ _shlint()
 				;;
 		esac
 	done < "$PROJECTCONF"
+	if [ ! -n "$subdirs" ]; then
+		_error "Could not locate directories to analyze"
+		return $?
+	fi
 	for subdir in $subdirs; do
 		[ -d "../$subdir" ] || continue
-		for filename in $($FIND "../$subdir" -name '*.sh' | $SORT); do
+		for filename in $($FIND "../$subdir" -type f -a -name '*.sh' | $SORT); do
 			_shlint_file "$filename"
 			if [ $? -eq 0 ]; then
 				echo "$filename:"
 			else
 				echo "$PROGNAME: $filename: FAIL" 1>&2
-				ret=2
+				res=2
 			fi
 		done
 	done
-	return $ret
+	return $res
 }
 
 _shlint_file()
@@ -88,7 +96,7 @@ _shlint_file()
 		esac
 	done < "$filename"
 	if [ $warn -ne 0 ]; then
-		_warning "$filename: return instead of exit in the global scope"
+		_error "$filename: return instead of exit in the global scope"
 	fi
 	return 0
 }
@@ -106,28 +114,31 @@ _debug()
 }
 
 
-#usage
-_usage()
-{
-	echo "Usage: $PROGNAME [-c] target" 1>&2
-	return 1
-}
-
-
-#warning
-_warning()
+#error
+_error()
 {
 	echo "$PROGNAME: $@" 1>&2
 	return 2
 }
 
 
+#usage
+_usage()
+{
+	echo "Usage: $PROGNAME [-c] target..." 1>&2
+	return 1
+}
+
+
 #main
 clean=0
-while getopts "cP:" name; do
+while getopts "cO:P:" name; do
 	case "$name" in
 		c)
 			clean=1
+			;;
+		O)
+			export "${OPTARG%%=*}"="${OPTARG#*=}"
 			;;
 		P)
 			#XXX ignored for compatibility
@@ -139,14 +150,24 @@ while getopts "cP:" name; do
 	esac
 done
 shift $((OPTIND - 1))
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
 	_usage
 	exit $?
 fi
-target="$1"
 
 #clean
 [ $clean -ne 0 ] && exit 0
 
 exec 3>&1
-_shlint > "$target"
+ret=0
+while [ $# -gt 0 ]; do
+	target="$1"
+	dirname="${target%/*}"
+	shift
+
+	if [ -n "$dirname" -a "$dirname" != "$target" ]; then
+		$MKDIR -- "$dirname"				|| ret=$?
+	fi
+	_shlint > "$target"					|| ret=$?
+done
+exit $ret
